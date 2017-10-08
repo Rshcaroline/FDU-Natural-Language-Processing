2a0a35ca618d8ecba1092f0809177a5b4acc16ff
Rshcaroline <rshcaroline@163.com>
2017-10-09 00:25:26 +0800

    corpus preprocessing

--- spell_correction.py
+++ spell_correction.py
@@ -1,12 +1,40 @@
 import numpy as np
+from nltk.corpus import brown
 
 def preprocessing():
+    # store the vocabulary to a list
     vocabpath = './vocab.txt'
     vocabfile = open(vocabpath, 'r')
     vocab = []
     for line in vocabfile:
         vocab.append(line[:-1])  # subtract the '\n'
 
+    # preprocessing the corpus and generate the count-file of n-gram
+    corpus_text = brown.sents(categories='news')
+    gram_count = {}
+    for sents in corpus_text:
+        # remove string.punctuation
+        for words in sents[::]:  # use [::] to remove the continuous ';' ';'
+            if (words in ['\'\'', '``', ',', '--', ';', ':', '(', ')', '&', '\'', '!', '?', '.']):  sents.remove(words)
+        # count the n-gram
+        for n in range(1, 4):
+            if (len(sents) <= n):  # 'This sentence is too short!'
+                continue
+            else:
+                for i in range(n, len(sents)):
+                    gram = sents[i - n: i]
+                    key = ''
+                    for words in gram:
+                        key = key + words + ' '
+                    key = key[:-1]  # remove the last ' '
+                    if (key in gram_count):  # use dict's hash
+                        gram_count[key] += 1
+                    else:
+                        gram_count[key] = 1
+
+    return vocab, gram_count
+
 if __name__ == '__main__':
-    preprocessing()
+    vocab, gram_count = preprocessing()
+
 
