d30fac01d5e2d5dba39e8ca6ef22ce6b9a0605f5
Rshcaroline <rshcaroline@163.com>
2017-10-28 15:42:17 +0800

    initialize

---
+++ scorer.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Scorer for the Stock Value Prediction
+ - @Chen Ting
+
+Your predictions should be saved in result.txt,
+where prediction is in {'+1', '-1'}.
+Each line contains one prediction.
+
+"""
+
+f = open('result.txt','r')
+predictions = [i.strip() for i in f.readlines()]
+f.close()
+
+f = open('test.txt','r')
+true_value = [i.split()[0] for i in f.readlines()]
+f.close()
+
+n = len(true_value)
+TP = len([i for i in range(n) if true_value[i] == '+1' and predictions[i] == '+1'])
+FN = len([i for i in range(n) if true_value[i] == '-1' and predictions[i] == '+1'])
+FP = len([i for i in range(n) if true_value[i] == '+1' and predictions[i] == '-1'])
+TN = len([i for i in range(n) if true_value[i] == '-1' and predictions[i] == '-1'])
+
+recall = TP/(TP+FN)
+precision = TP/(TP+FP)
+accuracy = (TP+TN)/n
+error_rate = 1-accuracy
+F1 = 2 * precision * recall/(precision+recall)
+
+print('recall :'+ str(recall))
+print('precision :'+ str(precision))
+print('accuracy :'+ str(accuracy))
+print('F1 score :'+ str(F1))
