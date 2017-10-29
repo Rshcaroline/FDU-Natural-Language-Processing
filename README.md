fe3fdde8344cbe5ea3bc015004085473536f7e8d
Rshcaroline <rshcaroline@163.com>
2017-10-29 11:23:41 +0800

    baseline 88%?

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FDU-NLP-Stock-Market-Prediction
 My second project in NLP class. More details please referred to README.md
+
+an idea! title is more important than contents!
--- stock prediction.py
+++ stock prediction.py
@@ -6,27 +6,14 @@
 # @File    : stock prediction.py
 # @Software: PyCharm
 
-
-# Requirements
-
-# You need to implement the Naïve Bayes model that maximizes the jointly likelihood of word features
-# and target labels for text classification.
-# At least three other classification models provided by NLTK should be tested and reported.
-
-
-# Input
-# A group of financial news about a target stock that publish in the same day.
-
-# Output
-# Predict whether the price of the target stock will go up or down.
-# 1. ‘+1’: price will go up
-# 2. ‘-1’: price will go down
-
-# Save your answer in “result.txt”.
-# Each line should contain one prediction inline with instances provided in “test.txt”.
-
 import codecs
+import jieba
+import nltk
+import random
+import time
+import pickle
 
+# read txt files
 def IOTxt():
     f = codecs.open('news.txt', 'r', 'utf8')
     news = [eval(i) for i in f.readlines()]
@@ -42,9 +29,80 @@ def IOTxt():
 
     return news, train, test
 
+# preprocess train and test set, along with their labels, get group news together
+def NewsGroup(news, train, test):
+    train_group = []
+    for k in range(0, len(train)):
+        print('Processing train ', k)
+        train_id = train[k][1].split(',')
+        train_content = []
+        for item in news:
+            for i in range(1, len(train_id)):
+                if int(item['id']) == int(train_id[i]):  # don't use item['id'] == int(news_id[i]) because it's string
+                    train_content.extend(list(jieba.cut(item['content'], cut_all=True)))
+        train_group.append((train_content, train[k][0]))  # news + label (['new1','','news2',''],'+1')
+
+    test_group = []
+    for k in range(0, len(test)):
+        print('processing test ', k)
+        test_id = train[k][1].split(',')
+        test_content = []
+        for item in news:
+            for i in range(1, len(test_id)):
+                if int(item['id']) == int(test_id[i]):
+                    test_content.extend(list(jieba.cut(item['content'], cut_all=True)))
+        test_group.append((test_content, train[k][0]))
+
+    pickle.dump(train_group, open('/Users/caroline/news_group.txt', 'wb'))
+    pickle.dump(test_group, open('/Users/caroline/test_group.txt', 'wb'))
+
+    return train_group, test_group
+
+# feature extraction
+def TextFeatures(text):
+    features = {}
+    # features['last_word'] = text[-1]
+    features['first_word'] = text[0]
+    features['length'] = len(text)
+
+    return  features
+
+def PrepareSets(train_group, test_group):
+    random.shuffle(train_group)
+
+    train_feature_sets = [(TextFeatures(text), label) for (text, label) in train_group]
+    test_feature_sets = [(TextFeatures(text), label) for (text, label) in test_group]
+    # train_set, test_set = train_feature_sets, test_feature_sets
+
+    return train_feature_sets, test_feature_sets
+
 if __name__ == '__main__':
 
-    news, train, test = IOTxt()
+    start = time.time()
 
-    print(news[0], train[0], test[0])
+    # load data
+    news, train, test = IOTxt()
+    train_group = pickle.load(open('/Users/caroline/news_group.txt', 'rb'))
+    test_group = pickle.load(open('/Users/caroline/test_group.txt', 'rb'))
+    # train_group, test_group = NewsGroup(news, train, test)
+
+    #prepare the training set and test set
+    train_set, test_set = PrepareSets(train_group, test_group)
+
+    # train a classifier
+    classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+    # test the classifier
+    resultpath = './result.txt'
+    resultfile = open(resultpath, 'w')
+    for item in test_set:
+        resultfile.write(classifier.classify(item[0]) + '\n')
+    resultfile.close()
+
+    print(nltk.classify.accuracy(classifier, test_set))
+    print(classifier.show_most_informative_features(10))
+
+    stop = time.time()
+    print('TIME:', str(stop-start) + '\n')
+
 
