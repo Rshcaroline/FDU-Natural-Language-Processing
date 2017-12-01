fd1d8df2bd8fcec4c19853e0d542d86769e326de
Rshcaroline <rshcaroline@163.com>
2017-12-01 18:44:54 +0800

    prepare

---
+++ extraction.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/1 下午6:43
+# @Author  : Shihan Ran
+# @Site    : 
+# @File    : extraction.py
+# @Software: PyCharm
+# @Description: Use sequence labeling models for Chinese event extraction.
+
